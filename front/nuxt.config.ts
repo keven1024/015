f8fea458c6b8b8a8dd5ac8108231c9be4c5102ff
keven1024 <keven@fudaoyuan.icu>
2025-03-18 17:57:59 +0800

    feat(front): update Nuxt configuration and dependencies for PWA support

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,6 +1,90 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+        dir: "ltr",
+      },
+      title: "Home",
+      titleTemplate: "%s - PWA App",
+      link: [
+        {
+          rel: "manifest",
+          href: "/manifest.json",
+        },
+        {
+          rel: "shortcut icon",
+          href: "/favicon.ico",
+        },
+        {
+          rel: "apple-touch-icon",
+          href: "/apple-touch-icon.png",
+          sizes: "180x180",
+        },
+      ],
+      meta: [
+        {
+          name: "application-name",
+          content: "PWA App",
+        },
+        {
+          name: "description",
+          content: "Best PWA app in the world!",
+        },
+        {
+          name: "apple-mobile-web-app-capable",
+          content: "yes",
+        },
+        {
+          name: "apple-mobile-web-app-status-bar-style",
+          content: "default",
+        },
+        {
+          name: "apple-mobile-web-app-title",
+          content: "My Awesome PWA App",
+        },
+        {
+          name: "format-detection",
+          content: "telephone=no",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:title",
+          content: "My Awesome PWA App",
+        },
+        {
+          property: "og:description",
+          content: "Best PWA app in the world!",
+        },
+        {
+          property: "og:site:name",
+          content: "PWA App",
+        },
+        {
+          name: "twitter:card",
+          content: "website",
+        },
+        {
+          name: "twitter:title",
+          content: "My Awesome PWA App",
+        },
+        {
+          name: "twitter:description",
+          content: "Best PWA app in the world!",
+        },
+        {
+          name: "theme-color",
+          content: "#FFFFFF",
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt',],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@serwist/nuxt'],
+  serwist: {},
 })
