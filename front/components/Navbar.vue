87388c4a0c47420f5984d83d540490887178b8f3
keven1024 <keven@fudaoyuan.icu>
2025-04-12 16:22:36 +0800

    feat(front): update Navbar component with motion effects and add new dependencies

--- front/components/Navbar.vue
+++ front/components/Navbar.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="flex flex-row bg-white/10 backdrop-blur-xl p-2 rounded-full">
         <div v-for="item in routes" :key="item.key" 
-        :class="cx('flex flex-row text-sm px-4 py-2 font-bold rounded-full cursor-pointer', 
-            item?.key === type && 'bg-black/10'
-        )"
+        class="flex flex-row text-sm px-4 py-2 font-bold rounded-full cursor-pointer relative"
         @click="()=>{
             router.push({
                 query: {
@@ -13,6 +11,7 @@
             })
         }"
         >
+          <motion.div v-if="item?.key === type" layoutId="navbar-active" class="absolute inset-0 rounded-full w-full h-full bg-black/10"/>
           {{ item.name }}
         </div>
     </div>
@@ -20,6 +19,7 @@
 
 <script setup lang="ts">
 import { cx } from 'class-variance-authority'
+import { motion } from "motion-v"
 const routes = [
     { name: '文件', key: 'file' },
     { name: '文本', key: 'text' }
