4d598803659621452b1178feb7f05cd273b22628
keven1024 <keven@fudaoyuan.icu>
2025-04-13 22:06:54 +0800

    feat(front): enhance Tiptap component with additional extensions and update styles for improved text editing experience

--- front/components/Tiptap.vue
+++ front/components/Tiptap.vue
@@ -1,10 +1,11 @@
 <template>
-    <editor-content :editor="editor" />
+    <editor-content :editor="editor" class="prose prose-sm bg-white/50 rounded-md p-2 [&>*]:outline-none" />
 </template>
 
 <script setup lang="ts">
 import { Editor, EditorContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
+import { Markdown } from 'tiptap-markdown';
 
 const props = defineProps<{
     modelValue: string
@@ -17,10 +18,10 @@ const editor = ref<Editor | undefined>(undefined)
 onMounted(() => {
   editor.value = new Editor({
     content: props.modelValue,
-    extensions: [StarterKit],
+    extensions: [StarterKit, Markdown],
     onUpdate: () => {
         // HTML
-        emit('update:modelValue', editor.value?.getHTML() ?? '')
+        emit('update:modelValue', editor.value?.storage?.markdown?.getMarkdown() ?? '')
       }
   })
 })
