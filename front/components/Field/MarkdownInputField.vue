f4990f05e0d3c97c271e34b4aa5e2f62cd3cc394
keven1024 <keven@fudaoyuan.icu>
2025-04-18 22:19:55 +0800

    refactor(front): simplify MarkdownInputField by removing unnecessary label and placeholder props

--- front/components/Field/MarkdownInputField.vue
+++ front/components/Field/MarkdownInputField.vue
@@ -1,14 +1,6 @@
 <template>
-    <Field :name="props.name" v-slot="{ field }">
-        <div class="flex flex-col gap-2">
-            <Label v-if="props.label">{{ props.label }}</Label>
-            <div class="border rounded-md">
-                <Tiptap 
-                    :modelValue="field.value" 
-                    @update:modelValue="field.onChange" 
-                />
-            </div>
-        </div>
+    <Field :name="name" v-slot="{ field }">
+        <Tiptap :modelValue="field.value" @update:modelValue="field.onChange" />
     </Field>
 </template>
 
@@ -17,7 +9,5 @@ import Tiptap from '@/components/Tiptap.vue'
 
 const props = defineProps<{
     name: string
-    label?: string
-    placeholder?: string
 }>()
 </script>
