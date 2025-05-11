9ff1161544d4b96401f2e6858860103b7da5f34e
keven1024 <99848979+keven1024@users.noreply.github.com>
2025-05-11 11:06:07 +0800

    chore(front): update dependencies in package.json and pnpm-lock.yaml, add spark-md5 and dayjs, and integrate vue-sonner for notifications

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,7 +1,11 @@
+<script lang="ts" setup>
+import { Toaster } from 'vue-sonner'
+</script>
 <template>
   <div class="h-screen w-screen ">
     <GlobalDrawer />
     <img class="w-full h-full object-cover absolute inset-0 -z-[1]" src="https://fuwari.vercel.app/_astro/demo-banner.DFyx781H_Z1gN7UP.webp" />
+    <Toaster position="top-center" richColors closeButton  />
     <div class=" h-full w-full flex flex-col items-center lg:p-10 p-5">
       <Navbar />
       <slot />
