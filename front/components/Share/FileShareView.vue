7564a7de0a29b96e912dafafc72d3a98c40744b8
keven1024 <keven@fudaoyuan.icu>
2025-05-15 17:07:24 +0800

    feat(front): add FileShareView component for file download functionality

---
+++ front/components/Share/FileShareView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import AsyncButton from '@/components/ui/button/AsyncButton.vue'
+const props = defineProps<{
+    data: any
+}>()
+const router = useRouter()
+
+
+const handleDownload = async () => {
+    const { id } = props?.data || {}
+    const data = await $fetch<any>(`/api/download`, {
+        method: 'POST',
+        body: {
+            share_id: id
+        }
+    })
+    const { token } = data?.data || {}
+    if (!token) {
+        return
+    }
+    (window as any)?.open(`/api/download?token=${token}`)
+}
+
+</script>
+
+<template>
+    <div class="flex flex-col gap-5 items-center">
+        <h1>下载文件</h1>
+        <FilePreviewView :value="props?.data" />
+        <div>
+            <AsyncButton @click="handleDownload">下载</AsyncButton>
+        </div>
+    </div>
+</template>
