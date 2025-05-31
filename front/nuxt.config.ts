fd92a0c4936a4b9bdd904764a6e310e55456fd41
keven1024 <keven@fudaoyuan.icu>
2025-05-31 16:24:33 +0800

    chore(front): update nuxt.config.ts and package.json for improved configuration and dependencies

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,105 +1,28 @@
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: "en",
-        dir: "ltr",
-      },
-      title: "Home",
-      titleTemplate: "%s - PWA App",
-      link: [
-        {
-          rel: "manifest",
-          href: "/manifest.json",
-        },
-        {
-          rel: "shortcut icon",
-          href: "/favicon.ico",
-        },
-        {
-          rel: "apple-touch-icon",
-          href: "/apple-touch-icon.png",
-          sizes: "180x180",
-        },
-      ],
-      meta: [
-        {
-          name: "application-name",
-          content: "PWA App",
-        },
-        {
-          name: "description",
-          content: "Best PWA app in the world!",
-        },
-        {
-          name: "apple-mobile-web-app-capable",
-          content: "yes",
-        },
-        {
-          name: "apple-mobile-web-app-status-bar-style",
-          content: "default",
-        },
-        {
-          name: "apple-mobile-web-app-title",
-          content: "My Awesome PWA App",
-        },
-        {
-          name: "format-detection",
-          content: "telephone=no",
-        },
-        {
-          property: "og:type",
-          content: "website",
-        },
-        {
-          property: "og:title",
-          content: "My Awesome PWA App",
-        },
-        {
-          property: "og:description",
-          content: "Best PWA app in the world!",
-        },
-        {
-          property: "og:site:name",
-          content: "PWA App",
-        },
-        {
-          name: "twitter:card",
-          content: "website",
-        },
-        {
-          name: "twitter:title",
-          content: "My Awesome PWA App",
-        },
-        {
-          name: "twitter:description",
-          content: "Best PWA app in the world!",
-        },
-        {
-          name: "theme-color",
-          content: "#FFFFFF",
-        },
-      ],
-    },
-  },
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   modules: [
-    '@vueuse/nuxt',
-    '@serwist/nuxt',
-    'motion-v/nuxt',
+    "@vueuse/nuxt", // '@serwist/nuxt',
+    "motion-v/nuxt",
     "nuxt-lucide-icons",
     "shadcn-nuxt",
-    '@vee-validate/nuxt',
-    '@pinia/nuxt',
+    "@vee-validate/nuxt",
+    "@pinia/nuxt",
+    "@nuxt/image",
   ],
-  serwist: {},
+  // serwist: {},
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-})
+  nitro: {
+    routeRules: {
+      "/api/**": { proxy: "http://127.0.0.1:1323/**" },
+    },
+  },
+  devServer: {
+    port: 5000,
+  },
+});
