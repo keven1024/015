4bcab3ba5ba315b87d9cb38dd2379832e501542e
keven1024 <keven@fudaoyuan.icu>
2025-06-02 12:46:13 +0800

    feat(backend): add CopyFile function for file duplication utility

--- worker/internal/utils/file.go
+++ worker/internal/utils/file.go
@@ -45,3 +45,20 @@ func GetUploadDirPath() (string, error) {
 	}
 	return uploadPath, nil
 }
+
+func CopyFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, sourceFile)
+	return err
+}
