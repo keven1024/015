ddcf53d456a34dfb1d6645a3e3459c00f412bb86
keven1024 <keven@fudaoyuan.icu>
2025-06-06 20:01:45 +0800

    fix: update default layout image source to a new dynamic URL for improved content delivery

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -8,7 +8,7 @@ await useSeo();
     <Toaster position="top-center" richColors closeButton />
     <img
       class="w-full h-full object-cover absolute inset-0 -z-[1]"
-      src="https://fuwari.vercel.app/_astro/demo-banner.DFyx781H_Z1gN7UP.webp"
+      src="https://img.fudaoyuan.icu/api/1/random/?scale_min=1.5&webp=true&md=false&format=302"
     />
     <div
       class="h-full w-full flex flex-col items-center lg:p-10 p-5 overflow-y-auto"
