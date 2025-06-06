a0de112853c1ef6a64e33faad8f7cec4f4be8648
keven1024 <keven@fudaoyuan.icu>
2025-06-06 20:23:06 +0800

    feat: add error component with localized back to home button for improved user experience

---
+++ front/error.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+import { useI18n } from "vue-i18n";
+import { Button } from "~/components/ui/button";
+
+const { t } = useI18n();
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <NuxtLayout>
+    <div
+      class="rounded-xl p-5 bg-white/50 backdrop-blur-xl w-full lg:w-200 flex flex-col items-center justify-center min-h-[50vh] mt-5 gap-10"
+    >
+      <div class="font-bold text-5xl">{{ error?.statusCode }}</div>
+      <Button @click="handleError">{{ t("btn.backToHome") }}</Button>
+    </div>
+  </NuxtLayout>
+</template>
