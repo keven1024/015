88b8daa5dfddf524ff9325344d4853f6e3c1cd83
keven <keven@fudaoyuan.icu>
2025-06-22 12:41:51 +0800

    feat: 修改 useSeo 以支持从环境变量获取 SEO 元数据，增强服务器端渲染能力

--- front/composables/useSeo.ts
+++ front/composables/useSeo.ts
@@ -4,22 +4,29 @@ type UseSeoProps = {
 }
 const useSeo = async (props: UseSeoProps = {}) => {
     const { head, seo } = props || {}
-    const { data } = await useFetch<any>('/config')
-    const seoMeta = computed(() => data.value?.data)
-    const { title } = head || {}
-    useHead({
-        ...head,
-        title: title ? `${title} - ${seoMeta?.value?.site_title}` : seoMeta?.value?.site_title,
-    })
-    useSeoMeta({
-        ...seo,
-        title: seoMeta?.value?.site_title,
-        description: seoMeta?.value?.site_desc,
-        ogTitle: seoMeta?.value?.site_title,
-        ogDescription: seoMeta?.value?.site_desc,
-        // ogImage: seoMeta?.value?.site_url,
-        // twitterCard: 'summary_large_image',
-    })
+    const seoMeta = ref<any>()
+    if (import.meta.server) {
+        const { SITE_TITLE, SITE_DESC, SITE_URL } = process.env || {}
+        seoMeta.value = {
+            site_title: SITE_TITLE,
+            site_desc: SITE_DESC,
+            site_url: SITE_URL,
+        }
+        const { title } = head || {}
+        useHead({
+            ...head,
+            title: title ? `${title} - ${seoMeta?.value?.site_title}` : seoMeta?.value?.site_title,
+        })
+        useSeoMeta({
+            ...seo,
+            title: seoMeta?.value?.site_title,
+            description: seoMeta?.value?.site_desc,
+            ogTitle: seoMeta?.value?.site_title,
+            ogDescription: seoMeta?.value?.site_desc,
+            // ogImage: seoMeta?.value?.site_url,
+            // twitterCard: 'summary_large_image',
+        })
+    }
     return
 }
 
