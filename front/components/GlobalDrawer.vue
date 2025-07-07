9fd4943bb6135a96116406eab3bcd077e89c8fca
keven1024 <keven@fudaoyuan.icu>
2025-07-07 11:20:42 +0800

    fix: update GlobalDrawer component to use direct drawer reference for improved state management

--- front/components/GlobalDrawer.vue
+++ front/components/GlobalDrawer.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import {
-  Drawer,
-  DrawerContent,
-} from '@/components/ui/drawer'
+import { Drawer, DrawerContent } from '@/components/ui/drawer'
 import { createVNode } from 'vue'
 import useStore from '@/composables/useStore'
 
@@ -12,22 +9,26 @@ const currentDrawer = computed(() => drawer?.value?.[drawer?.value?.length - 1])
 
 const render = computed(() => currentDrawer?.value?.render)
 const Children = () =>
-  createVNode(render.value, {
-    hide: () => store?._set('drawer', drawer?.value?.slice(0, -1)),
-  })
-
+    createVNode(render.value, {
+        hide: () => store?._set('drawer', drawer?.value?.slice(0, -1)),
+    })
 </script>
 
 <template>
-  <Drawer :open="!!store?._get('drawer')?.[store?._get('drawer')?.length - 1]" @update:open="(open) => {
-    if (!open && drawer?.length > 0) {
-      store?._set('drawer', drawer?.slice(0, -1))
-    }
-  }">
-    <DrawerContent>
-      <div class="mx-auto w-full max-w-sm pb-10">
-        <Children />
-      </div>
-    </DrawerContent>
-  </Drawer>
+    <Drawer
+        :open="!!drawer?.[drawer?.length - 1]"
+        @update:open="
+            (open) => {
+                if (!open && drawer?.length > 0) {
+                    store?._set('drawer', drawer?.slice(0, -1))
+                }
+            }
+        "
+    >
+        <DrawerContent>
+            <div class="mx-auto w-full max-w-sm pb-10">
+                <Children />
+            </div>
+        </DrawerContent>
+    </Drawer>
 </template>
