8392b2645592d3618293b7b941cce8ca420700b0
keven1024 <keven@fudaoyuan.icu>
2025-07-28 18:17:04 +0800

    feat(front): add Pixi component integration and update dependencies for improved functionality

---
+++ front/components/Pixi.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Application } from 'vue3-pixi'
+import { useElementSize } from '@vueuse/core'
+
+export interface PixiExpose {
+    app: Application
+    width: number
+    height: number
+    contentHeight: number
+    ref: HTMLElement
+}
+
+const containerRef = ref<HTMLElement>()
+const app = ref<Application>()
+const { width: windowWidth, height: windowHeight } = useWindowSize()
+const { width: containerWidth, height: containerHeight } = useElementSize(containerRef)
+const width = computed(() => Math.min(containerWidth.value, windowWidth.value))
+const contentHeight = ref(0)
+const height = computed(() => {
+    if (!contentHeight.value) return Math.min(containerHeight.value, windowHeight.value)
+    if (containerHeight.value > contentHeight.value) return contentHeight.value
+    return contentHeight.value
+})
+const resolution = computed(() => window?.devicePixelRatio || 1)
+
+watchEffect(() => {
+    if (app.value?.app?.renderer) {
+        app.value?.app?.renderer?.resize(width.value, height.value)
+    }
+})
+
+defineExpose({
+    app,
+    width,
+    height,
+    contentHeight,
+    ref: containerRef,
+})
+</script>
+
+<template>
+    <div ref="containerRef" class="w-full h-full">
+        <Application :width="width" :height="height" :resolution="resolution" ref="app" autoDensity antialias>
+            <slot />
+        </Application>
+    </div>
+</template>
--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,40 +1,45 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
-  css: ["@/assets/css/main.css"],
-  modules: [
-    // '@serwist/nuxt',
-    "@vueuse/nuxt",
-    "motion-v/nuxt",
-    "nuxt-lucide-icons",
-    "shadcn-nuxt",
-    "@vee-validate/nuxt",
-    "@pinia/nuxt",
-    "@nuxt/image",
-    "@nuxtjs/i18n",
-  ],
-  // serwist: {},
-  i18n: {
-    strategy: "no_prefix",
-    defaultLocale: "en",
-    locales: [
-      { code: "zh-CN", name: "中文(简体)", file: "zh-CN.json" },
-      { code: "en", name: "English", file: "en.json" },
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    css: ['@/assets/css/main.css'],
+    modules: [
+        // '@serwist/nuxt',
+        '@vueuse/nuxt',
+        'motion-v/nuxt',
+        'nuxt-lucide-icons',
+        'shadcn-nuxt',
+        '@vee-validate/nuxt',
+        '@pinia/nuxt',
+        '@nuxt/image',
+        '@nuxtjs/i18n',
+        'vue3-pixi-nuxt',
     ],
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  nitro: {
-    routeRules: {
-      "/api/**": {
-        proxy: process.env.API_BASE_URL || "http://127.0.0.1:1323/**",
-      },
+    // serwist: {},
+    i18n: {
+        strategy: 'no_prefix',
+        defaultLocale: 'en',
+        locales: [
+            { code: 'zh-CN', name: '中文(简体)', file: 'zh-CN.json' },
+            { code: 'en', name: 'English', file: 'en.json' },
+        ],
     },
-  },
-  devServer: {
-    port: 5000,
-  },
-});
+    vite: {
+        transformMixedEsModules: true,
+        plugins: [tailwindcss()],
+        optimizeDeps: {
+            include: ['eventemitter3'],
+        },
+    },
+    nitro: {
+        routeRules: {
+            '/api/**': {
+                proxy: process.env.API_BASE_URL || 'http://127.0.0.1:1323/**',
+            },
+        },
+    },
+    devServer: {
+        port: 5000,
+    },
+})
