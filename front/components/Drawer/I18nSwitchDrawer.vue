249b9f2350bdd4a226d98d83a23f8777ce733f5d
keven1024 <keven@fudaoyuan.icu>
2026-04-07 22:53:13 +0800

    feat(front): update I18nSwitchDrawer to support new locale structure and add German, French, Japanese, Korean, and Traditional Chinese translations

--- front/components/Drawer/I18nSwitchDrawer.vue
+++ front/components/Drawer/I18nSwitchDrawer.vue
@@ -1,23 +1,15 @@
 <script setup lang="ts">
 import { cx } from 'class-variance-authority'
+import type { Locale } from '@intlify/core-base'
 
 const props = defineProps<{
     hide: () => void
 }>()
 
-const { availableLocales, setLocale, locale: currentLocale, t } = useI18n()
+const { locales, setLocale, locale: currentLocale, t } = useI18n()
 
-const localeMap = {
-    'zh-CN': '简体中文',
-    en: 'English',
-    // 'ja': '日本語',
-    // 'ko': '한국어',
-    // 'fr': 'Français',
-    // 'de': 'Deutsch',
-}
-
-const switchLocale = async (locale: string) => {
-    await setLocale(locale as keyof typeof localeMap)
+const switchLocale = async (locale: Locale) => {
+    await setLocale(locale)
     props.hide()
 }
 </script>
@@ -26,12 +18,12 @@ const switchLocale = async (locale: string) => {
     <div class="flex flex-col gap-1 py-2">
         <div class="text-xl font-bold mb-3">{{ t('i18n.switchLocale') }}</div>
         <div
-            v-for="locale in availableLocales"
-            :key="locale"
-            :class="cx('rounded-md hover:bg-black/10 p-2 cursor-pointer', currentLocale === locale && 'bg-black/10 font-bold')"
-            @click="() => switchLocale(locale)"
+            v-for="locale in locales"
+            :key="locale.code"
+            :class="cx('rounded-md hover:bg-black/10 p-2 cursor-pointer', currentLocale === locale.code && 'bg-black/10 font-bold')"
+            @click="() => switchLocale(locale.code)"
         >
-            {{ localeMap?.[locale as keyof typeof localeMap] }}
+            {{ locale.name }}
         </div>
     </div>
 </template>
