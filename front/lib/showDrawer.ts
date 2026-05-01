b023876e0f18dfad0e60a2a1c7ac97308f116682
keven1024 <keven@fudaoyuan.icu>
2026-05-01 10:48:15 +0800

    feat(front): update GlobalDrawer component to support async close handling and improve drawer visibility management

--- front/lib/showDrawer.ts
+++ front/lib/showDrawer.ts
@@ -1,4 +1,5 @@
 import type { DrawerItem } from '~/components/GlobalDrawer.vue'
+import asyncWait from './asyncWait'
 
 type DrawerProps<T = unknown> = Pick<DrawerItem<T>, 'render'>
 
@@ -6,12 +7,14 @@ function showDrawer<T = unknown>(props: DrawerProps<T>): Promise<T | undefined> 
     const key = Math.random().toString(36).slice(2, 8)
     return new Promise((res) => {
         const { render } = props || {}
-        const onClose = (data?: T) => {
-            store.drawer = (store.drawer ?? [])?.filter((item) => item.key !== key)
+        const store = useStore()
+        const onClose = async (data?: T) => {
+            store.drawer = store.drawer?.map((d) => (d.key === key ? { ...d, visible: false } : d))
+            await asyncWait(500)
+            store.drawer = (store.drawer ?? []).filter((d) => d.key !== key)
             res(data)
         }
-        const store = useStore()
-        store.drawer = [...(store.drawer || []), { render, onClose, key }]
+        store.drawer = [...(store.drawer || []), { render, onClose, key, visible: true }]
     })
 }
 
