f956130b4f5c4cd9ffcf48e82f92806c513111ba
keven1024 <keven@fudaoyuan.icu>
2026-05-24 14:03:49 +0800

    chore(dependencies): replace lucide-vue-next with @lucide/vue in package.json and update lint workflow for improved dependency management

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from '@tailwindcss/vite'
+import { defineNuxtConfig } from 'nuxt/config'
 import getApiBaseUrl from './lib/getApiBaseUrl'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
@@ -9,7 +10,6 @@ export default defineNuxtConfig({
         // '@serwist/nuxt',
         '@vueuse/nuxt',
         'motion-v/nuxt',
-        'nuxt-lucide-icons',
         'shadcn-nuxt',
         '@vee-validate/nuxt',
         '@pinia/nuxt',
--- front/pages/s/[id].vue
+++ front/pages/s/[id].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { LucideAlertCircle } from 'lucide-vue-next'
+import { LucideAlertCircle } from '@lucide/vue'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
 import dayjs from 'dayjs'
